fe52ea62f3387c8561cb26941b87747862950ecd
Jonathan <jonathanestebangalan@gmail.com>
2021-08-24 12:39:47 +0200

    Diseño del menú mobile terminado | Iniciado el diseño del menú Desktop

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const btnMenu = document.getElementById('btn-menu')
 const btnClose = document.getElementById('btn-close')
+const btnSubMenu = document.getElementById('btn-submenu')
+const btnSubMarcas = document.getElementById('btn-marcas')
 const nav = document.getElementById('nav')
 
 btnMenu.addEventListener('click', () => {
@@ -9,4 +11,13 @@ btnMenu.addEventListener('click', () => {
 btnClose.addEventListener('click', () => {
   nav.classList.toggle('nav--deactive')
   nav.classList.toggle('nav--active')
+})
+
+btnSubMenu.addEventListener('click', () => {
+  btnSubMenu.classList.toggle('menu__models--deactive')
+  btnSubMenu.classList.toggle('menu__models--active')
+})
+btnSubMarcas.addEventListener('click', () => {
+  btnSubMarcas.classList.toggle('menu__marcas--deactive')
+  btnSubMarcas.classList.toggle('menu__marcas--active')
 })
