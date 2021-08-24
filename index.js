3c3dd1c1e06cbaa185fb5cc422f12cbab4df4c23
Jonathan <jonathanestebangalan@gmail.com>
2021-08-24 16:35:00 +0200

    Mejorado el header y el diseño del menu Desktop

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-const btnMenu = document.getElementById('btn-menu')
-const btnClose = document.getElementById('btn-close')
+const btnMenu = document.getElementById("btn-menu");
+const btnClose = document.getElementById("btn-close");
 const btnSubMenu = document.getElementById('btn-submenu')
 const btnSubMarcas = document.getElementById('btn-marcas')
-const nav = document.getElementById('nav')
+const nav = document.getElementById("nav");
 
-btnMenu.addEventListener('click', () => {
-  nav.classList.toggle('nav--active')
-  nav.classList.toggle('nav--deactive')
-})
-btnClose.addEventListener('click', () => {
-  nav.classList.toggle('nav--deactive')
-  nav.classList.toggle('nav--active')
-})
-
-btnSubMenu.addEventListener('click', () => {
-  btnSubMenu.classList.toggle('menu__models--deactive')
-  btnSubMenu.classList.toggle('menu__models--active')
-})
-btnSubMarcas.addEventListener('click', () => {
-  btnSubMarcas.classList.toggle('menu__marcas--deactive')
-  btnSubMarcas.classList.toggle('menu__marcas--active')
-})
+if (window.screen.width < 900) {
+  btnMenu.addEventListener("click", () => {
+    nav.classList.toggle("nav--active");
+    nav.classList.toggle("nav--deactive");
+  });
+  btnClose.addEventListener("click", () => {
+    nav.classList.toggle("nav--deactive");
+    nav.classList.toggle("nav--active");
+  });
+  
+  btnSubMenu.addEventListener("click", () => {
+    btnSubMenu.classList.toggle("menu__models--deactive");
+    btnSubMenu.classList.toggle("menu__models--active");
+  });
+  btnSubMarcas.addEventListener("click", () => {
+    btnSubMarcas.classList.toggle("menu__marcas--deactive");
+    btnSubMarcas.classList.toggle("menu__marcas--active");
+  });
+}
